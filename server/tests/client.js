63db26748180137a905a9a08ab0eb544e2fc520d
xavier-kong <kongxianchengxavier@gmail.com>
2021-09-14 14:55:39 +0800

    changed table to single line

--- server/tests/client.js
+++ server/tests/client.js
@@ -10,15 +10,7 @@ const pgclient = new Client({
 
 pgclient.connect();
 
-const table = `CREATE TABLE users (
-    user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY,
-      username VARCHAR(100) NOT NULL,
-      passwordhash VARCHAR(200) NOT NULL,
-    creation_date TIMESTAMP NOT NULL,
-    UNIQUE(user_uid),
-    UNIQUE(username),
-    UNIQUE(passwordhash)
-  )`
+const table = 'CREATE TABLE users (    user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY, username VARCHAR(100) NOT NULL, passwordhash VARCHAR(200) NOT NULL, creation_date TIMESTAMP NOT NULL,    UNIQUE(user_uid), UNIQUE(username), UNIQUE(passwordhash))'
 
 pgclient.query(table, (err, res) => {
     if (err) throw err
