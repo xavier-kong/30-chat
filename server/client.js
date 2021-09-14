4d510e8a49fa36cd6b71cd7756bb431329ddefd1
xavier-kong <kongxianchengxavier@gmail.com>
2021-09-14 15:06:03 +0800

    trying own table

--- server/client.js
+++ server/client.js
@@ -1,21 +1,3 @@
-// const { Client } = require('pg');
-
-// const pgclient = new Client({
-//     host: process.env.POSTGRES_HOST,
-//     port: process.env.POSTGRES_PORT,
-//     user: 'postgres',
-//     password: 'postgres',
-//     database: 'postgres'
-// });
-
-// pgclient.connect();
-
-// const table = 'CREATE TABLE users (    user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY, username VARCHAR(100) NOT NULL, passwordhash VARCHAR(200) NOT NULL, creation_date TIMESTAMP NOT NULL,    UNIQUE(user_uid), UNIQUE(username), UNIQUE(passwordhash))'
-
-// pgclient.query(table, (err, res) => {
-//     if (err) throw err
-// });
-
 const { Client } = require('pg');
 
 const pgclient = new Client({
@@ -28,20 +10,8 @@ const pgclient = new Client({
 
 pgclient.connect();
 
-const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+const table = 'CREATE TABLE users (    user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY, username VARCHAR(100) NOT NULL, passwordhash VARCHAR(200) NOT NULL, creation_date TIMESTAMP NOT NULL,    UNIQUE(user_uid), UNIQUE(username), UNIQUE(passwordhash))'
 
 pgclient.query(table, (err, res) => {
     if (err) throw err
-});
-
-pgclient.query(text, values, (err, res) => {
-    if (err) throw err
-});
-
-pgclient.query('SELECT * FROM student', (err, res) => {
-    if (err) throw err
-    console.log(err, res.rows) // Print the data in student table
-    pgclient.end()
 });
