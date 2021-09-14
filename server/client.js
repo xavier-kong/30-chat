980e150aa3a40365d7f6bb394def3d9eb5cfb9b8
xavier-kong <kongxianchengxavier@gmail.com>
2021-09-14 15:56:14 +0800

    again db name

--- server/client.js
+++ server/client.js
@@ -5,7 +5,7 @@ const pgclient = new Client({
     port: process.env.POSTGRES_PORT,
     user: 'postgres',
     password: 'postgres',
-    database: 'postgres_test'
+    database: 'postgres'
 });
 
 pgclient.connect();
