c2adc105c7d15f053afb969cfea8d7221a6eb7f9
xavier-kong <kongxianchengxavier@gmail.com>
2021-09-14 15:53:18 +0800

    error

--- server/client.js
+++ server/client.js
@@ -5,13 +5,15 @@ const pgclient = new Client({
     port: process.env.POSTGRES_PORT,
     user: 'postgres',
     password: 'postgres',
-    database: 'postgres'
+    database: 'postgres_test'
 });
 
 pgclient.connect();
 
 const uid = 'CREATE EXTENSION uuid-ossp'
-const table = 'CREATE TABLE users (    user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY, username VARCHAR(100) NOT NULL, passwordhash VARCHAR(200) NOT NULL, creation_date TIMESTAMP NOT NULL,    UNIQUE(user_uid), UNIQUE(username), UNIQUE(passwordhash))'
+// const table = 'CREATE TABLE users (    user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY, username VARCHAR(100) NOT NULL, passwordhash VARCHAR(200) NOT NULL, creation_date TIMESTAMP NOT NULL,    UNIQUE(user_uid), UNIQUE(username), UNIQUE(passwordhash))'
+
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
 
 pgclient.query(uid, (err, res) => {
     if (err) {
