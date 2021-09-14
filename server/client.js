f94232cdd1cbf5a1ed6d4cd98bc85bb9c0679634
xavier-kong <kongxianchengxavier@gmail.com>
2021-09-14 16:34:04 +0800

    e

--- server/client.js
+++ server/client.js
@@ -10,13 +10,13 @@ const pgclient = new Client({
 
 pgclient.connect();
 
-const uid = 'CREATE EXTENSION uuid-ossp'
-const table = 'CREATE TABLE users (    user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY, username VARCHAR(100) NOT NULL, passwordhash VARCHAR(200) NOT NULL, creation_date TIMESTAMP NOT NULL,    UNIQUE(user_uid), UNIQUE(username), UNIQUE(passwordhash))'
+const uid = 'CREATE EXTENSION $1'
+const table = 'CREATE TABLE users ( user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY, username VARCHAR(100) NOT NULL, passwordhash VARCHAR(200) NOT NULL, creation_date TIMESTAMP NOT NULL, UNIQUE(user_uid), UNIQUE(username), UNIQUE(passwordhash))'
 // const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
 // const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
 // const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
 
-pgclient.query(uid, (err, res) => {
+pgclient.query(uid, ['uuid-ossp'], (err, res) => {
     if (err) throw err
 });
 
