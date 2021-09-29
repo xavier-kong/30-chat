699801801afe68db97e0ed498d200db42cd61e2d
xavier-kong <kongxianchengxavier@gmail.com>
2021-09-29 14:43:51 +0800

    installed socket.io for server

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -11,3 +11,10 @@ const Chat = () => {
 }
 
 export default Chat
+
+
+//socket io learning links
+//https://www.tutorialspoint.com/socket.io/socket.io_chat_application.htm
+//https://github.com/moniba/socketio-chatroom
+//https://blog.crowdbotics.com/build-chat-app-with-nodejs-socket-io/
+//https://www.section.io/engineering-education/creating-a-real-time-chat-app-with-react-socket-io-with-e2e-encryption/
