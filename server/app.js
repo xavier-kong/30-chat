e70731a80d8f178258314ed755835e1a8f0ff6d5
xavier-kong <kongxianchengxavier@gmail.com>
2021-10-19 16:44:37 +0800

    added manual send of js files

--- server/app.js
+++ server/app.js
@@ -15,6 +15,11 @@ app.use(morgan('tiny'))
 
 
 app.use(express.static(path.join(__dirname, 'build/static')))
+
+app.get('/static/js/:name', (req,res) => {
+    res.sendFile(path.join(__dirname, `build/static/js/${req.params.name}`))
+})
+
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.use(middleware.tokenExtractor)
