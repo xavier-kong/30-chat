3b5c2f779bf355b36580debeaa7250159db08024
xavier-kong <kongxianchengxavier@gmail.com>
2021-10-29 17:57:46 +0800

    added fix for 404 react router

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const middleware = require('./utils/middleware')
 const usersRouter = require('./routers/usersRouter')
 const groupsRouter = require('./routers/groupsRouter')
+const path = require('path')
 
 const app = express()
 
@@ -17,6 +18,10 @@ app.use(express.static('build'))
 app.use(middleware.tokenExtractor)
 app.use('/api/users', usersRouter)
 app.use('/api/groups', groupsRouter)
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname+'/build/index.html'))
+})
 
 app.use(middleware.unknownEndpoint)
 
