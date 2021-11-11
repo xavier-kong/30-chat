d0e472064b154f2310cc961dfb51b2650872120c
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-11 13:04:41 +0800

    conditional url

--- client/src/services/loginPost.js
+++ client/src/services/loginPost.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 const url = process.env.NODE_ENV === 'test' ? process.env.REACT_APP_PROD : process.env.REACT_APP_BUILD
 
 const loginPost = async (username, password) => {
+    console.log(url)
     const res = await axios.post(url, {
         username: username,
         password: password
