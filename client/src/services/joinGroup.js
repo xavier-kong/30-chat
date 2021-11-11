d81900f2434118563537e4bf799d91f448439da2
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-11 13:37:12 +0800

    editted relative links

--- client/src/services/joinGroup.js
+++ client/src/services/joinGroup.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const joinGroup = async (url, group_name, passphrase, username, config) => {
-    await axios.post(url, {
+    await axios.post(`${url}api/groups/join`, {
         group_name,
         passphrase,
         username
--- client/src/services/loginPost.js
+++ client/src/services/loginPost.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const loginPost = async (url, username, password) => {
     console.log(url)
-    const res = await axios.post(url, {
+    const res = await axios.post(`${url}api/users/login`, {
         username: username,
         password: password
       })
