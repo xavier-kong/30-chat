4ca7d07a410d984075f85a713139443d2fbc6b24
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-11 12:57:17 +0800

    added log url

--- client/src/services/loginPost.js
+++ client/src/services/loginPost.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 const url = process.env.BMODE ? process.env.BUILD : process.env.PROD
 
 const loginPost = async (username, password) => {
+    console.log(url)
     const res = await axios.post(url, {
         username: username,
         password: password
