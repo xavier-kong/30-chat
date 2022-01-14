a41c3273e30aa5a5567d9da70dd0ca593ad0fb2b
xavier-kong <kongxianchengxavier@gmail.com>
2022-01-14 16:33:51 +0800

    planning for Chat page

---
+++ client/src/pages/Chat.js
@@ -0,0 +1,4 @@
+// if user? display logout button and maybe "welcome {username}" in navbar
+
+// for pc, main screen divide into 2, left side show exisitng groups, right side join group (copy sign in style)
+// for mobile, make it toggle between both
