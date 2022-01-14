e4f7cb4ae58a18ab079bd694d3dbbc62ed174475
xavier-kong <kongxianchengxavier@gmail.com>
2022-01-14 16:34:23 +0800

    should've been in Rooms not Chat

--- client/src/pages/Chat.js
+++
@@ -1,4 +0,0 @@
-// if user? display logout button and maybe "welcome {username}" in navbar
-
-// for pc, main screen divide into 2, left side show exisitng groups, right side join group (copy sign in style)
-// for mobile, make it toggle between both
