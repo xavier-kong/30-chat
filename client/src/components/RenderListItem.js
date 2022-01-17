521e330ef0c7909f08194aa0c106b0aa6d042e67
xavier-kong <kongxianchengxavier@gmail.com>
2022-01-17 09:44:58 +0800

    added RenderListItem component

---
+++ client/src/components/RenderListItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { ListItemButton } from "@mui/material";
+import Countdown from 'react-countdown';
+
+const RenderListItem = ({ name, exp }) => {
+
+    const renderer = ({ hours, minutes, seconds, completed }) => {
+        if (completed) {
+            return <span>This group has expired</span>;
+        } else {
+            return <span>Expires in {hours}:{minutes}:{seconds}</span>;
+        }
+        };
+
+    return (
+        <ListItem component="div" disablePadding>
+            <ListItemButton>
+                <ListItemText 
+                    primary={name} 
+                    secondary={<Countdown date={new Date(exp)} renderer={renderer} />}
+                />
+            </ListItemButton>
+        </ListItem>
+    )
+}
+
+export default RenderListItem
